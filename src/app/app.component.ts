30259039afd0cfb4463d1c9461e9e41a375a6310
parthmehta17 <pg.mehta17@gmail.com>
2020-05-17 13:49:29 +0530

    code commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Calculator';
+
+  public Num1 : number;
+  public Num2 : number;
+  public result : number;
+
+  add()
+  {
+    this.result = this.Num1 + this.Num2;
+  }
+  sub()
+  {
+    this.result = this.Num1 - this.Num2;
+  }
+  mul()
+  {
+    this.result = this.Num1 * this.Num2;
+  }
+  div()
+  {
+    this.result = this.Num1 / this.Num2;
+  }
 }
